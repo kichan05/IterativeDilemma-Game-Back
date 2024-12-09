f80248f018840635784bae6e5dde6e261900e4d8
바키찬 <ckstmznf@naver.com>
2024-12-09 18:58:45 +0900

    Switch Case로 변경

--- server.js
+++ server.js
@@ -18,56 +18,60 @@ wss.on('connection', (ws) => {
 
   ws.on('message', (message) => {
     const json = JSON.parse(message);
+    let roomId
 
-    if (json.type === socketType.ROOM_CREATE) {
-      const roomId = getRandom(1001, 9999);
+    switch (json.type) {
+      case socketType.ROOM_CREATE:
+        roomId = getRandom(1001, 9999);
 
-      ws.roomId = roomId;
-      ws.name = "호스트"
-      rooms[roomId] = {roomId, host: ws, users: []};
-
-      console.log("방 생성 요청 끗", rooms);
-
-      ws.send(JSON.stringify({
-        type: socketType.ROOM_CREATE_REQ,
-        room: {roomId, users: rooms[roomId].users.map(u => u.name)},
-        name: ws.name
-      }))
-    } else if (json.type === socketType.ROOM_JOIN) {
-      const roomId = json.roomId;
-      const name = json.name;
-      if (roomId in rooms) {
-        console.log(`${name}이(가) ${roomId}방에 가입했습니다.`)
         ws.roomId = roomId;
-        ws.name = name;
-        rooms[roomId].users.push(ws);
+        ws.name = "호스트"
+        rooms[roomId] = {roomId, host: ws, users: []};
+
+        console.log("방 생성 요청 끗", rooms);
 
         ws.send(JSON.stringify({
-          type: socketType.ROOM_JOIN_SUCCESS,
+          type: socketType.ROOM_CREATE_REQ,
           room: {roomId, users: rooms[roomId].users.map(u => u.name)},
-          name: name,
+          name: ws.name
         }))
-        rooms[roomId].users.forEach((client) => {
-          if (client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify({
-              type: socketType.ROOM_DATA_UPDATE,
-              room: {roomId, users: rooms[roomId].users.map(u => u.name)}
-            }))
-          }
-        });
+        break;
+      case socketType.ROOM_JOIN:
+        roomId = json.roomId;
+        const name = json.name;
+        if (roomId in rooms) {
+          console.log(`${name}이(가) ${roomId}방에 가입했습니다.`)
+          ws.roomId = roomId;
+          ws.name = name;
+          rooms[roomId].users.push(ws);
 
-        rooms[roomId].host.send(JSON.stringify({
-          type: socketType.ROOM_DATA_UPDATE,
-          room: {roomId, users: rooms[roomId].users.map(u => u.name)}
-        }))
-      }
+          ws.send(JSON.stringify({
+            type: socketType.ROOM_JOIN_SUCCESS,
+            room: {roomId, users: rooms[roomId].users.map(u => u.name)},
+            name: name,
+          }))
+          rooms[roomId].users.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+              client.send(JSON.stringify({
+                type: socketType.ROOM_DATA_UPDATE,
+                room: {roomId, users: rooms[roomId].users.map(u => u.name)}
+              }))
+            }
+          });
+
+          rooms[roomId].host.send(JSON.stringify({
+            type: socketType.ROOM_DATA_UPDATE,
+            room: {roomId, users: rooms[roomId].users.map(u => u.name)}
+          }))
+        }
+        break
     }
-  });
 
-  ws.on('close', () => {
-    // rooms[ws.roomId].remove(ws);
-    console.log('클라이언트가 연결을 종료');
+    ws.on('close', () => {
+      // rooms[ws.roomId].remove(ws);
+      console.log('클라이언트가 연결을 종료');
+    });
   });
-});
+})
 
 app.use(express.static('public'));
--- socketType.js
+++ socketType.js
@@ -2,9 +2,11 @@ const socketType = {
   ROOM_CREATE: "createRoom",
   ROOM_CREATE_REQ: "roomCreateReq",
   ROOM_JOIN: "joinRom",
-  ROOM_JOIN_SUCCESS : "roomJoinSuccess",
-  // ROOM_USER_JOIN: "roomUserJoin"
-  ROOM_DATA_UPDATE : "roomDataUpdate"
+  ROOM_JOIN_SUCCESS: "roomJoinSuccess",
+  ROOM_DATA_UPDATE: "roomDataUpdate",
+  RTC_OFFER: "rtcOffer",
+  RTC_ANSWER: "rtcAnswer",
+  ICE_CANDIDATE: "icecandidate",
 }
 
 module.exports = socketType;
