ab7aa266972eb31dc16993a1011b1e328c3272d9
바키찬 <ckstmznf@naver.com>
2024-12-09 18:05:14 +0900

    Room 데이터 구조 변경

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const server = app.listen(8080, () => {
   console.log('Server run at http://localhost:8080');
 });
 
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocket.Server({server});
 
 wss.on('connection', (ws) => {
   console.log('클라이언트 연결');
@@ -19,49 +19,49 @@ wss.on('connection', (ws) => {
   ws.on('message', (message) => {
     const json = JSON.parse(message);
 
-    if(json.type === socketType.ROOM_CREATE) {
+    if (json.type === socketType.ROOM_CREATE) {
       const roomId = getRandom(1001, 9999);
 
-      rooms[roomId] = [];
       ws.roomId = roomId;
       ws.name = "호스트"
-      rooms[roomId].push(ws);
+      rooms[roomId] = {roomId, host: ws, users: []};
 
-      console.log("방 생성 요청 끗", rooms)
+      console.log("방 생성 요청 끗", rooms);
 
       ws.send(JSON.stringify({
         type: socketType.ROOM_CREATE_REQ,
-        roomId,
+        room: {roomId, users: rooms[roomId].users.map(u => u.name)},
+        name: ws.name
       }))
-    }
-    else if(json.type === socketType.ROOM_JOIN) {
+    } else if (json.type === socketType.ROOM_JOIN) {
       const roomId = json.roomId;
       const name = json.name;
-      if(roomId in rooms) {
+      if (roomId in rooms) {
         console.log(`${name}이(가) ${roomId}방에 가입했습니다.`)
-        rooms[roomId].push(ws);
         ws.roomId = roomId;
-        ws.name = name
+        ws.name = name;
+        rooms[roomId].users.push(ws);
 
         ws.send(JSON.stringify({
           type: socketType.ROOM_JOIN_SUCCESS,
-          roomId, name
+          room: {roomId, users: rooms[roomId].users.map(u => u.name)},
+          name: name,
         }))
-        rooms[roomId].forEach((client) => {
-          if(client.readyState === WebSocket.OPEN) {
+        rooms[roomId].users.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
             client.send(JSON.stringify({
               type: socketType.ROOM_DATA_UPDATE,
-              users : rooms[roomId].map(u => u.name)
+              room: {roomId, users: rooms[roomId].users.map(u => u.name)}
             }))
           }
-        })
+        });
+
+        rooms[roomId].host.send(JSON.stringify({
+          type: socketType.ROOM_DATA_UPDATE,
+          room: {roomId, users: rooms[roomId].users.map(u => u.name)}
+        }))
       }
     }
-    // wss.clients.forEach((client) => {
-    //   if (client !== ws && client.readyState === WebSocket.OPEN) {
-    //     client.send(message);
-    //   }
-    // });
   });
 
   ws.on('close', () => {
