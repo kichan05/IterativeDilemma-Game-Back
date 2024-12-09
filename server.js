eed233f4507b52a93f6089794725b18b2df6263f
바키찬 <ckstmznf@naver.com>
2024-12-09 15:33:21 +0900

    방 생성 완료

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const WebSocket = require('ws');
+const {getRandom} = require("./utlis");
+const socketType = require("./socketType");
+
+let rooms = {}
 
 const app = express();
+
 const server = app.listen(8080, () => {
   console.log('Server run at http://localhost:8080');
 });
@@ -12,13 +17,27 @@ wss.on('connection', (ws) => {
   console.log('클라이언트 연결');
 
   ws.on('message', (message) => {
-    console.log('받은 메시지:', message);
+    const json = JSON.parse(message);
 
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
+    if(json.type === socketType.ROOM_CREATE) {
+      const roomId = getRandom(1001, 9999);
+
+      rooms[roomId] = [];
+      ws.roomId = roomId;
+      rooms[roomId].push(ws);
+
+      console.log("방 생성 요청 끗", roomId)
+
+      ws.send(JSON.stringify({
+        type: socketType.ROOM_CREATE_REQ,
+        roomId,
+      }))
+    }
+    // wss.clients.forEach((client) => {
+    //   if (client !== ws && client.readyState === WebSocket.OPEN) {
+    //     client.send(message);
+    //   }
+    // });
   });
 
   ws.on('close', () => {
---
+++ socketType.js
@@ -0,0 +1,7 @@
+const socketType = {
+  ROOM_CREATE: "createRoom",
+  ROOM_CREATE_REQ: "roomCreateReq",
+  ROOM_JOIN: "joinRom",
+}
+
+module.exports = socketType;
