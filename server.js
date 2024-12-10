5bb09ce254a3f4a897c153f75b79407f20be14d7
바키찬 <ckstmznf@naver.com>
2024-12-10 15:51:38 +0900

    socket handle 함수화

--- server.js
+++ server.js
@@ -5,6 +5,73 @@ const socketType = require("./socketType");
 
 let rooms = {}
 
+function getRoomData(roomId) {
+  return {roomId, users: rooms[roomId].users.map(u => u.name)};
+}
+
+function handleRoomCreate(ws) {
+  const roomId = getRandom(1001, 9999);
+
+  ws.roomId = roomId;
+  ws.isHost = true;
+  ws.name = "호스트"
+  rooms[roomId] = {roomId, host: ws, users: []};
+
+  ws.send(JSON.stringify({
+    type: socketType.ROOM_CREATE_REQ,
+    room: getRoomData(roomId),
+    name: ws.name
+  }))
+}
+
+function handleRoomJoin(ws, json) {
+  const {roomId, name} = json;
+
+  if (roomId in rooms) {
+    ws.roomId = roomId;
+    ws.name = name;
+    ws.isHost = false;
+    rooms[roomId].users.push(ws);
+
+    ws.send(JSON.stringify({
+      type: socketType.ROOM_JOIN_SUCCESS,
+      room: getRoomData(roomId),
+      name: name,
+    }))
+    const users = rooms[roomId].users;
+    [...users, rooms[roomId].host]
+      .forEach((client) => {
+        client.send(JSON.stringify({
+          type: socketType.ROOM_DATA_UPDATE,
+          room : getRoomData(roomId),
+        }))
+      });
+  }
+}
+
+function handleRTCOffer(json) {
+  const roomId = json.roomId;
+  rooms[roomId].host.send(JSON.stringify(json));
+}
+
+function handleRTCAnswer(json) {
+  const roomId = json.roomId;
+  const target = rooms[roomId].users.filter(u => u.name = json.name)[0];
+  target.send(JSON.stringify(json));
+}
+
+function handleICECandidateSend(ws, json) {
+  const {roomId, isHost} = ws;
+  const room = rooms[roomId]
+  const targets = isHost ? room.users : [room.host];
+
+  targets.forEach((u) => {
+    u.send(JSON.stringify({
+      ...json, type: socketType.ICE_CANDIDATE_RECEIVE
+    }))
+  })
+}
+
 const app = express();
 
 const server = app.listen(8080, () => {
@@ -18,76 +85,23 @@ wss.on('connection', (ws) => {
 
   ws.on('message', (message) => {
     const json = JSON.parse(message);
-    let roomId, isHost, targets;
+    // let roomId, isHost, targets;
 
     switch (json.type) {
       case socketType.ROOM_CREATE:
-        roomId = getRandom(1001, 9999);
-
-        ws.roomId = roomId;
-        ws.isHost = true;
-        ws.name = "호스트"
-        rooms[roomId] = {roomId, host: ws, users: []};
-
-        console.log("방 생성 요청 끗", rooms);
-
-        ws.send(JSON.stringify({
-          type: socketType.ROOM_CREATE_REQ,
-          room: {roomId, users: rooms[roomId].users.map(u => u.name)},
-          name: ws.name
-        }))
+        handleRoomCreate(ws);
         break;
       case socketType.ROOM_JOIN:
-        roomId = json.roomId;
-        const name = json.name;
-        if (roomId in rooms) {
-          console.log(`${name}이(가) ${roomId}방에 가입했습니다.`)
-          ws.roomId = roomId;
-          ws.name = name;
-          ws.isHost = false;
-          rooms[roomId].users.push(ws);
-
-          ws.send(JSON.stringify({
-            type: socketType.ROOM_JOIN_SUCCESS,
-            room: {roomId, users: rooms[roomId].users.map(u => u.name)},
-            name: name,
-          }))
-          rooms[roomId].users.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
-              client.send(JSON.stringify({
-                type: socketType.ROOM_DATA_UPDATE,
-                room: {roomId, users: rooms[roomId].users.map(u => u.name)}
-              }))
-            }
-          });
-
-          rooms[roomId].host.send(JSON.stringify({
-            type: socketType.ROOM_DATA_UPDATE,
-            room: {roomId, users: rooms[roomId].users.map(u => u.name)}
-          }))
-        }
+        handleRoomJoin(ws, json);
         break
       case socketType.RTC_OFFER:
-        roomId = json.roomId;
-        console.log(roomId)
-        rooms[roomId].host.send(message);
+        handleRTCOffer(json);
         break;
       case socketType.RTC_ANSWER:
-        roomId = json.roomId;
-        const target = rooms[roomId].users.filter(u => u.name = json.name)[0];
-        console.log(target)
-        target.send(message);
+        handleRTCAnswer(json);
         break;
       case socketType.ICE_CANDIDATE_SEND:
-        roomId = ws.roomId;
-        isHost = ws.isHost;
-
-        targets = isHost ? rooms[roomId].users : [rooms[roomId].host];
-        targets.forEach((u) => {
-          u.send(JSON.stringify({
-            ...json, type: socketType.ICE_CANDIDATE_RECEIVE
-          }))
-        })
+        handleICECandidateSend(ws, json);
         break;
     }
 
